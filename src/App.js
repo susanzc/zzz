63bff65bc95fbb0a35fa3769e1506375f959af16
Susan Chen <susanszc@gmail.com>
2019-01-27 11:29:31 -0800

    added math function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Button, Modal, Input, ModalHeader, ModalFooter, ModalBody, ListGroup, ListGroupItem } from 'reactstrap';
+import { Button, Modal, Input, Card, CardBody, 
+  Collapse, ModalHeader, ModalFooter, ModalBody, ListGroup, 
+  ListGroupItem, FormGroup, Label } from 'reactstrap';
 import TimePicker from 'rc-time-picker';
 import moment from 'moment';
 
@@ -20,6 +22,8 @@ const spotifyApi = new SpotifyWebApi({
 class App extends Component {
   constructor(props) {
     super(props);
+    let mathAlarm = new Audio('http://www.gravomaster.com/alarm/sounds/Alarme.wav');
+    mathAlarm.loop = true;
     this.state = { token: "", 
     user: "", 
     modalOpen: false, 
@@ -28,7 +32,13 @@ class App extends Component {
     time: null,
     selectedPl: null,
     displayText: [],
-    audio: null}
+    audio: null,
+    mathLvl: "easy",
+    equation: null,
+    answer: null,
+    currSoln: "",
+    mathAlarm: mathAlarm,
+    musicOpen: true}
     this.loadUser = this.loadUser.bind(this);
     this.authenticationComplete = this.authenticationComplete.bind(this);
     this.tokenSaved = this.tokenSaved.bind(this);
@@ -40,6 +50,12 @@ class App extends Component {
     this.removeAlarm = this.removeAlarm.bind(this);
     this.checkAlarms = this.checkAlarms.bind(this);
     this.checkTrack = this.checkTrack.bind(this);
+    this.toggleMusic = this.toggleMusic.bind(this);
+    this.setMathLvl = this.setMathLvl.bind(this);
+    this.playTrack = this.playTrack.bind(this);
+    this.getMathQuestion = this.getMathQuestion.bind(this);
+    this.checkMath = this.checkMath.bind(this);
+    this.updateSoln = this.updateSoln.bind(this);
   }
 
   componentDidMount() {
@@ -81,6 +97,10 @@ class App extends Component {
 
   }
 
+  toggleMusic(open) {
+    this.setState({musicOpen: open});
+  }
+
   tokenSaved() {
     return window.location.href.includes("success") && sessionStorage.getItem("token");
   }
@@ -111,8 +131,22 @@ class App extends Component {
     if (toCheck == this.state.track) {
       let audio = this.state.audio;
       audio.pause();
-      this.setState({track: null, audio: null, enteredText: track});
+      this.setState({track: null, audio: null, displayText: ""});
     }
+  }
+
+  updateSoln(event) {
+    this.setState({currSoln: event.target.value});
+  }
+  
+  checkMath() {
+    let soln = Number(this.state.currSoln);
+    if (soln == this.state.answer) {
+      this.setState({answer: null, equation: null});
+      let audio = this.state.mathAlarm;
+      audio.pause();
+    }
+    
   }
 
   loadUser() {
@@ -131,6 +165,11 @@ class App extends Component {
     this.setState({selectedPl: pl})
   }
 
+  setMathLvl(e) {
+    console.log(e.target.name);
+    this.setState({mathLvl: e.target.name});
+  }
+
   loadPlaylists() {
     let that = this;
     spotifyApi.getUserPlaylists()
@@ -140,8 +179,11 @@ class App extends Component {
       )
   }
   
-  addAlarm() {
-    let alarm = {time: this.state.time, pl: this.state.selectedPl};
+  addAlarm(isMusic) {
+    let alarm = {time: this.state.time, math: this.state.mathLvl, pl: null};
+    if (isMusic) {
+      alarm = {time: this.state.time, pl: this.state.selectedPl, math: null};
+    }
     let alarms = this.state.activeAlarms;
     alarms.push(alarm);
     alarms.sort((a, b) => a.time.isAfter(b.time) ? 1 : -1);
@@ -153,11 +195,73 @@ class App extends Component {
   checkAlarms(){
     let activeAlarms = this.state.activeAlarms;
     let newAlarms = [];
-    let that = this;
     for (let i = 0; i < activeAlarms.length; i++) {
       let alarm = activeAlarms[i];
       if (moment().isSameOrAfter(alarm.time)) {
-        spotifyApi.getPlaylist(alarm.pl.id)
+        if (alarm.pl == null) {
+          let audio = this.state.mathAlarm;
+          audio.play();
+          this.getMathQuestion(alarm.math);
+        }
+        else {
+          this.playTrack(alarm);
+        }
+      } else {
+        newAlarms.push(activeAlarms[i]);
+      }
+    }
+    this.setState({activeAlarms: newAlarms});
+  }
+
+  getMathQuestion(mathLvl) {
+    let operators = ['+', '-', '*'];
+    let operators2 = [['+', '*'], ['-', '*'], ['*', '-'], ['*', '+']];
+    let equation = "";
+    let answer = 0;
+    let a,b,c,d,e,ind;
+    switch (mathLvl) {
+      case "easy":
+        a = Math.floor(Math.random() * 10);
+        b = operators[Math.floor((Math.random()) * 99) / 50];
+        c = Math.floor(Math.random() * 10);
+        equation = a.toString() + " " + b + " "+ c.toString();
+        answer = eval(equation);
+        break;
+      case "moderate":
+        ind = operators2[Math.floor((Math.random() * 100)/26)];
+        a = Math.floor(Math.random() * 10);
+        b = ind[0];
+        c = Math.floor(Math.random() * 10);
+        d = ind[1];
+        e = Math.floor(Math.random() * 10);
+        equation = a.toString() + " " + b + " "+ c.toString() + " "+ d + " "+ e.toString();
+        answer = eval(equation);
+        break;
+      case "hard":
+        ind = operators2[Math.floor((Math.random() * 100)/26)];
+        a = Math.floor(Math.random() * 40 + 10);
+        b = ind[0];
+        c = Math.floor(Math.random() * 50 + 50);
+        d = ind[1];
+        e = Math.floor(Math.random() * 10);
+        equation = a.toString() + " " + b + " "+ c.toString() + " "+ d + " "+ e.toString();
+        answer = eval(equation);
+        break;
+      case "boss":
+        equation = "Compute the volume of the solid on the first octant (x, y, z >= 0) bounded by the planes y = x, x = 1 and z = x + y";
+        answer = 0.5;
+        break;
+      default: 
+        equation = "0";
+        answer = 0;
+        break;
+    }
+    this.setState({equation: equation, answer: answer});
+  }
+
+  playTrack(alarm) {
+    let that = this;
+    spotifyApi.getPlaylist(alarm.pl.id)
         .then(function(data) {
           let tracks = data.body.tracks.items;
           let track = tracks[Math.floor(Math.random()*tracks.length)].track;
@@ -174,11 +278,6 @@ class App extends Component {
         }, function(err) {
           console.log('Something went wrong!', err);
         });
-      } else {
-        newAlarms.push(activeAlarms[i]);
-      }
-    }
-    this.setState({activeAlarms: newAlarms});
   }
 
 
@@ -188,18 +287,34 @@ class App extends Component {
       let time = alarm.time._d.toLocaleTimeString();
       let end = time.length;
       time = time.substring(0, end - 6) + " " + time.substring(end - 2, end);
-    alarms.push(
-    <ListGroupItem style={{margin: 10, width: 500, borderStyle: 'dashed', borderWidth: '5px', borderColor: 'grey'}}>
-    <div style={{display: 'flex', color: 'dimgray'}}>
-        <div style={{width: 500}}>
-          <div style={{fontWeight: 'bold'}}>{time}</div>
-          <div style={{fontWeight: 'normal', fontSize: '16pt'}}>Playlist: {alarm.pl.name}</div>
+    if (alarm.pl == null) {
+      alarms.push(
+        <ListGroupItem style={{margin: 10, width: 500, borderStyle: 'dashed', borderWidth: '5px', borderColor: 'grey'}}>
+        <div style={{display: 'flex', color: 'dimgray'}}>
+            <div style={{width: 500}}>
+              <div style={{fontWeight: 'bold'}}>{time}</div>
+              <div style={{fontWeight: 'normal', fontSize: '16pt'}}>Math</div>
+            </div>
+            <div style={{paddingTop: '12px'}}>
+              <Button size="md" color="danger" onClick={()=>this.removeAlarm(alarm)}>Remove</Button>
+              </div>
         </div>
-        <div style={{paddingTop: '12px'}}>
-          <Button size="md" color="danger" onClick={()=>this.removeAlarm(alarm)}>Remove</Button>
+        </ListGroupItem>);
+    }
+    else {
+      alarms.push(
+      <ListGroupItem style={{margin: 10, width: 500, borderStyle: 'dashed', borderWidth: '5px', borderColor: 'grey'}}>
+      <div style={{display: 'flex', color: 'dimgray'}}>
+          <div style={{width: 500}}>
+            <div style={{fontWeight: 'bold'}}>{time}</div>
+            <div style={{fontWeight: 'normal', fontSize: '16pt'}}>Playlist: {alarm.pl.name}</div>
           </div>
-    </div>
-    </ListGroupItem>)
+          <div style={{paddingTop: '12px'}}>
+            <Button size="md" color="danger" onClick={()=>this.removeAlarm(alarm)}>Remove</Button>
+            </div>
+      </div>
+      </ListGroupItem>);
+      }
     });
     return alarms;
   }
@@ -233,6 +348,13 @@ class App extends Component {
             <div style={{float: 'left', padding: 12}}>{this.state.displayText}</div>
           </div>
           }
+          {this.state.equation != null && 
+          <div style={{width: 400}}>
+          <center><div style={{float: 'left', padding: 12}}>{this.state.equation}</div></center>
+          <Input type="text" onChange={this.updateSoln} name="math" id="math" placeholder="Enter Decimal Solution"/>
+          <div style={{padding: 7}}><Button color="info" onClick={this.checkMath}>Submit</Button></div>
+        </div>
+          }
         <div style={{marginTop: 50, color: "white", fontSize: '18pt', fontWeight: '600'}}>
          {this.state.activeAlarms.length == 0? "No Alarms" : 
          <ListGroup>{this.renderAlarms()}</ListGroup>}
@@ -253,16 +375,64 @@ class App extends Component {
             use12Hours
             inputReadOnly
           />
-          <div style={{marginTop: 20, marginBottom: 20}}>Select Playlist:</div>
+          <br/>
+          <div style={{padding: '12px 0px'}}>
+          <Button color="info" active={this.state.musicOpen} onClick={() => this.toggleMusic(true)}>Music</Button>{' '}
+          <Button color="info" active = {!this.state.musicOpen} onClick={() => this.toggleMusic(false)}>Math</Button>
+          </div>
+          <Collapse isOpen={this.state.musicOpen}>
+          <Card>
+            <CardBody>
+          <legend>Select Playlist:</legend>
           <div style={{height: 200, overflow: 'scroll'}}>
               <ListGroup>
               {this.renderPlaylists()}
               </ListGroup>
-            </div>
+          </div>
+          </CardBody>
+          </Card>
+          </Collapse>
+          <Collapse isOpen={!this.state.musicOpen}>
+          <Card>
+            <CardBody>
+            <FormGroup tag="fieldset">
+          <legend>Select Difficulty</legend>
+          <FormGroup check>
+            <Label check>
+              <Input type="radio" name="easy" checked={this.state.mathLvl == "easy"} 
+              onChange={this.setMathLvl} />{' '}
+              Easy
+            </Label>
+          </FormGroup>
+          <FormGroup check>
+            <Label check>
+              <Input type="radio" name="moderate" checked={this.state.mathLvl == "moderate"} 
+              onChange={this.setMathLvl}/>{' '}
+              Moderate
+            </Label>
+          </FormGroup>
+          <FormGroup check>
+            <Label check>
+              <Input type="radio" name="hard" checked={this.state.mathLvl == "hard"} 
+              onChange={this.setMathLvl} />{' '}
+              Hard
+            </Label>
+          </FormGroup>
+          <FormGroup check>
+            <Label check>
+              <Input type="radio" name="boss" checked={this.state.mathLvl == "boss"} 
+              onChange={this.setMathLvl} />{' '}
+              Lvl 100 Boss
+            </Label>
+          </FormGroup>
+        </FormGroup>
+            </CardBody>
+          </Card>
+          </Collapse>
           </ModalBody>
           <ModalFooter>
-            <Button color="primary" disabled={this.state.time == null || this.state.selectedPl == null} 
-            onClick={this.addAlarm}>Add</Button>{' '}
+            <Button color="primary" disabled={this.state.time == null || (this.state.musicOpen && this.state.selectedPl == null)} 
+            onClick={() => this.addAlarm(this.state.musicOpen)}>Add</Button>{' '}
             <Button color="secondary" onClick={this.toggleModal}>Cancel</Button>
           </ModalFooter>
         </Modal>
